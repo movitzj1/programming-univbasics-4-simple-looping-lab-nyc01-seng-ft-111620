b4198371ed0fbbe27e73d0c72f9d5b6edbcb9006
Justin Movitz <movitzj1@montclair.edu>
2020-10-18 17:51:00 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,27 @@
-# Write your methods here
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
+
+def loop_message_n_times(message, interger)
+  count = 0
+  while count < n do
+    puts message
+    count += 1
+  end
+end
+
+def output_array(array)
+  count = 0
+  while count < length.array do
+    puts array[count]
+    count += 1
+  end
+end
+  puts 
+  
+  
+  
